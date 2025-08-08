3cf6f232abb9c5924e5c7837955151a3524a612b
ENDERSON-MARIN <marinenderson1@gmail.com>
2025-08-08 17:34:13 -0300

    feat: add header component and logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { Header } from "@/components/commons/header";
 
 const Home = () => {
   return (
-    <div>
-      <h1 className="mt-5 text-2xl font-bold">Hello World</h1>
-      <Button>Bootcamp</Button>
-    </div>
+    <>
+      <Header />;
+    </>
   );
 };
 
